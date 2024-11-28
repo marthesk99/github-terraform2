e4bc8b80df06a56aa21af0e880256be28ad3c624
Marthe Solland Koch <marthe.s.koch@tisipfagskole.no>
2024-11-28 20:51:09 +0100

    add tls version

--- modul09/main.tf
+++ modul09/main.tf
@@ -15,6 +15,7 @@ resource "random_string" "random_string" {
 resource "azurerm_resource_group" "rg_web" {
   name     = local.rg_name
   location = var.location
+  min_tls_version = "TLS1_2"
 }
 
 # Create Storage Account
